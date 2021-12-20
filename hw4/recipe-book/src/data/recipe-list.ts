f84e73fa39821eb81834b6233c92fb555ee55057
NoVarlok <yakhtin.leonid@yandex.ru>
2021-12-20 22:18:20 +0300

    Add data

---
+++ hw4/recipe-book/src/data/recipe-list.ts
@@ -0,0 +1,38 @@
+import { Recipe } from "./recipe";
+
+export const initial_recipes: Recipe[] = [
+    {
+        name: "kekw",
+        ingredients: [
+            {
+                name: "k",
+                quantity: 2
+            },
+            {
+                name: 'e',
+                quantity: 1
+            },
+            {
+                name: 'w',
+                quantity: 1
+            }
+        ]
+    },
+    {
+        name: "zulul",
+        ingredients: [
+            {
+                name: "z",
+                quantity: 1
+            },
+            {
+                name: 'u',
+                quantity: 2
+            },
+            {
+                name: 'l',
+                quantity: 2
+            }
+        ]
+    }
+]
